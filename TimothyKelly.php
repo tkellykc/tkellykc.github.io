6cbf5f6a3a2ccbbdc3cd8195fc7f53179203f01e
tkellykc <40213922+tkellykc@users.noreply.github.com>
2019-01-22 15:22:53 -0600

    Add files via upload

---
+++ TimothyKelly.php
@@ -0,0 +1,18 @@
+<?php
+// Timothy Kelly
+// CSIS 279 - Yoast
+// Chapter 1 Project 1
+// This project displays a banner for a webpage featuring the Chinese Zodiac
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0
+Strict //EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head> // opens header element
+	<title>The Chinese Zodiac | A Code Deominstration for PHP</title> // page title
+	<meta charset="utf-8"> // sets character set
+</head> // closes header element
+<body> // opens body element
+<img src="http://www.crazyhats.org/Images/ChineseZodiacBanner.jpg" alt="Chinese Zodiac">
+</body> // closes body element
+</html> // closes header element
+?>
